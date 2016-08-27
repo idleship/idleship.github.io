8340875e463508203609f51a7f09cb01b48724af
zorbus77 <rocklegend77@gmail.com>
2016-08-27 15:34:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,22 +5,3 @@ function increase()
   document.getElementById("increase").innerHTML=i;
   return true;
 }
-
-var Building1Name = "Planet Miner";
-var Building1Cost = 25;
-var Building1PerSec = 1;
-var Building1Qty = 0;
-
-function Tick() {
-	increase = increase + (Building1Qty * Building1PerSec);
-	document.getElementById("increase").innerHTML = increase;
-}
-
-function BuyLemonadeStand() {
-	if (increase >= Building1Cost) {
-		increase = increase - Building1Cost;
-		Building1Qty = Building1Qty + 1;
-		document.getElementById("increase").innerHTML = increase;
-		document.getElementById("Building1Qty").innerHTML = Building1Qty;
-	}
-}
