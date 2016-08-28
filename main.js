9a5afcf992aaeeb6f860e96fbd3080bdbc5526ae
zorbus77 <rocklegend77@gmail.com>
2016-08-28 01:39:23 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,35 @@ function increase()
   document.getElementById("increase").innerHTML=i;
   return true;
 }
+var totalClicks = 0;
+
+var clickIncrement = 1;
+
+var autoClicker = {
+    amount: 0,
+    cost: 0,
+    increment: 1
+};
+
+var tick = 1000;
+
+/* Run the AutoClicker */
+var runAutoClicker = setInterval(function () {
+    totalClicks = totalClicks + (autoClicker.increment * autoClicker.amount);
+    document.getElementById("total_clicks").innerHTML = totalClicks;
+}, tick)
+
+/* Buy AutoClickers */
+$('#autoClickerBuy').click(function () {
+    if (totalClicks >= 10) {
+        totalClicks = totalClicks - 10;
+        autoClicker.amount++;
+        document.getElementById("autoClickers").innerHTML = autoClicker.amount;
+    }
+});
+
+/* Click to Increment */
+$('#click').click(function () {
+    totalClicks = totalClicks + clickIncrement;
+    document.getElementById("total_clicks").innerHTML = totalClicks;
+});
